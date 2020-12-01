ef97d7f40dbd4d2fbe8dd181fcd3986383b5dac1
shok1122 <shok1122@zohomail.com>
2020-12-01 12:30:49 +0000

    Replaced  to host name

--- network/scripts/createChannel.sh
+++ network/scripts/createChannel.sh
@@ -57,7 +57,7 @@ createChannel() {
 		sleep $DELAY
 		set -x
         echo $FABRIC_CFG_PATH
-		peer channel create -o localhost:7050 -c $CHANNEL_NAME --ordererTLSHostnameOverride orderer.xkeycloak.com -f ./channel-artifacts/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block --tls --cafile $ORDERER_CA >&log.txt
+		peer channel create -o orderer.xkeycloak.com:7050 -c $CHANNEL_NAME --ordererTLSHostnameOverride orderer.xkeycloak.com -f ./channel-artifacts/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block --tls --cafile $ORDERER_CA >&log.txt
 		res=$?
 		set +x
 		let rc=$res
@@ -100,7 +100,7 @@ updateAnchorPeers() {
 	while [ $rc -ne 0 -a $COUNTER -lt $MAX_RETRY ] ; do
     sleep $DELAY
     set -x
-		peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.xkeycloak.com -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls --cafile $ORDERER_CA >&log.txt
+		peer channel update -o orderer.xkeycloak.com:7050 --ordererTLSHostnameOverride orderer.xkeycloak.com -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls --cafile $ORDERER_CA >&log.txt
     res=$?
     set +x
 		let rc=$res
--- network/scripts/envVar.sh
+++ network/scripts/envVar.sh
@@ -32,18 +32,18 @@ setGlobals() {
     export CORE_PEER_LOCALMSPID="Org1MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.xkeycloak.com/users/Admin@org1.xkeycloak.com/msp
-    export CORE_PEER_ADDRESS=localhost:7051
+    export CORE_PEER_ADDRESS=peer0.org1.xkeycloak.com:7051
   elif [ $USING_ORG -eq 2 ]; then
     export CORE_PEER_LOCALMSPID="Org2MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.xkeycloak.com/users/Admin@org2.xkeycloak.com/msp
-    export CORE_PEER_ADDRESS=localhost:9051
+    export CORE_PEER_ADDRESS=peer0.org2.xkeycloak.com:9051
 
   elif [ $USING_ORG -eq 3 ]; then
     export CORE_PEER_LOCALMSPID="Org3MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG3_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.xkeycloak.com/users/Admin@org3.xkeycloak.com/msp
-    export CORE_PEER_ADDRESS=localhost:11051
+    export CORE_PEER_ADDRESS=peer0.org3.xkeycloak.com:11051
   else
     echo "================== ERROR !!! ORG Unknown =================="
   fi
